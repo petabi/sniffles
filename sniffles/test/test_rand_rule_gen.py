8b4c1d49ea718f3be3d329eb58f1a940744c3500
Tu Le <tuleph@gmail.com>
2015-07-02 16:37:22 -0700

    - Finished implementing all the test cases for rand-rule-generators

--- sniffles/test/test_rand_rule_gen.py
+++ sniffles/test/test_rand_rule_gen.py
@@ -98,4 +98,85 @@ class TestRandRuleGen(TestCase):
             self.assertTrue(val >= 0 and val <= 65535)
 
     def test_snort_rule_getRuleWithFormat(self):
-        pass
+        featureParser = FeatureParser()
+        line = "type=feature; name=alert; complexity_prob=100; " \
+               "ambiguity_list=[alert, log, dyn" \
+               "amic, drop, sdrop]"
+        testAlert = ['alert', 'log', 'dynamic', 'drop', 'sdrop']
+        featureParser.parseLine(line)
+
+        line = "type=protocol; name=proto; proto_list=[TCP,UDP];"
+        testProtocol = ['TCP', 'UDP']
+        featureParser.parseLine(line)
+
+        line = "type=ip; name=sip; complexity_prob=100; version=4;"
+        featureParser.parseLine(line)
+
+        line = "type=feature; name=sport; complexity_prob=100; " \
+               "ambiguity_list=[1500,1600,{1400,1300,1670}]"
+        featureParser.parseLine(line)
+
+        line = "type=ip; name=dip; complexity_prob=100; version=6;"
+        featureParser.parseLine(line)
+
+        line = "type=feature; name=dport; complexity_prob=100; amb" \
+               "iguity_list=[1250,[6000,7000]]"
+        featureParser.parseLine(line)
+
+        line = "type=content; name=content; regex=True"
+        featureParser.parseLine(line)
+
+        line = "type=feature; name=dir; complexity_prob=100; " \
+               "ambiguity_list=[->,<>];"
+        featureParser.parseLine(line)
+
+        myfeatures = featureParser.getFeatures()
+        myrules = generateRules(myfeatures, 1)
+        myrule = myrules[0]
+        myvals = myrule.split(";")
+        mymap = {}
+        for v in myvals:
+            v = v.strip()
+            if len(v) > 0:
+                v_list = v.split("=")
+                mymap[v_list[0]] = v_list[1]
+        self.assertTrue(mymap['alert'] in testAlert)
+        self.assertTrue(mymap['proto'] in testProtocol)
+
+        sip = mymap['sip']
+        self.assertTrue("/" in sip)
+        sipPart = sip.split("/")[0]
+        sipPart = sipPart.split(".")
+        self.assertEqual(len(sipPart), 4)
+        for i in range(0, 4):
+            val = int(sipPart[i])
+            self.assertTrue(val >= 0 and val <= 255)
+
+        dip = mymap['dip']
+        self.assertTrue("/" in dip)
+        dipPart = dip.split("/")[0]
+        dipPart = dipPart.split(":")
+        self.assertEqual(len(dipPart), 8)
+        for i in range(0, 8):
+            val = int(dipPart[i], 16)
+            self.assertTrue(val >= 0 and val <= 65535)
+
+        rule_list = generateRules(myfeatures, 1)
+
+        for rule in rule_list:
+
+            myvals = rule.split(";")
+            mymap = {}
+            for v in myvals:
+                v = v.strip()
+                if len(v) > 0:
+                    v_list = v.split("=")
+                    mymap[v_list[0]] = v_list[1]
+
+            result = getRuleWithFormat(rule,  "snort")
+            testStr = mymap["alert"] + " " + mymap["proto"] + " " + \
+                mymap["sip"] + " " + mymap["sport"] + " " + \
+                mymap["dir"] + " " + mymap["dip"] + " " + \
+                mymap["dport"] + ' ( content:"' + \
+                mymap["content"] + '"; sid:1;)'
+            self.assertEqual(str(result), testStr)
