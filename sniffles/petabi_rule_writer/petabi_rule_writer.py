eddfd13ab3d2767ad546b7e90cacaec992ae93bc
Moosuk Pyun <mskpyn@gmail.com>
2017-02-03 15:42:47 +0900

    Fixed bug when all background protocols are given a percentage and they don't add up to 100

--- sniffles/petabi_rule_writer/petabi_rule_writer.py
+++ sniffles/petabi_rule_writer/petabi_rule_writer.py
@@ -215,7 +215,8 @@ def formatBackgroundTrafficRule(background_traffic, protocol_dist=None):
 # Label protocol percentage with given distribution.
 # Wildcard (* symbol) is used to denote even percentage among wildcards.
 # Wildcard protocol will not be shown in the rule, but will be applied
-# in sniffles.
+# in sniffles. When all protocols are given a percentage and their sum
+# does not add up to 100, they are treated as ratio.
 # Output: Dictionary containing protocol percentage
 def protocolPercentage(protocol_dist):
     protocol_percent = OrderedDict()
@@ -225,9 +226,34 @@ def protocolPercentage(protocol_dist):
     i = 0
     while i < len(protocol_list):
         if protocol_dist[i] != "*":
-            protocol_percent[protocol_list[i]] = protocol_dist[i]
+            protocol_percent[protocol_list[i]] = int(protocol_dist[i])
         i += 1
+
+    # Change ratio to percentage for protocol if they don't add up to 100
+    if len(protocol_list) == len(protocol_percent):
+        protocol_sum = sumPercentage(protocol_percent)
+        while protocol_sum != 100:
+            for protocol in protocol_percent:
+                new_percent = protocol_percent[protocol] / protocol_sum * 100
+                protocol_percent[protocol] = int(new_percent)
+            protocol_sum = sumPercentage(protocol_percent)
+            if protocol_sum < 100:
+                rand_protocol = random.choice(protocol_list)
+                protocol_percent[rand_protocol] += 1
+            elif protocol_sum > 100:
+                rand_protocol = random.choice(protocol_list)
+                protocol_percent[rand_protocol] -= 1
+
     return protocol_percent
+
+
+# Simple adder for percentage value in dictionary
+def sumPercentage(protocol_percent):
+    protocol_sum = 0
+    for protocol in protocol_percent:
+        protocol_sum += protocol_percent[protocol]
+
+    return protocol_sum
 
 
 # Formats whole rule file to a petabi rule format.
