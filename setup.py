a3756c0497c8bf98969c49922309b088b37a4090
Min Sik Kim <msk@dolbo.net>
2018-08-17 09:23:11 -0700

    Bump version to 3.4.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import Extension, find_packages, setup
 
 setup(
     name='sniffles',
-    version='3.4.1',
+    version='3.4.2',
     description='Sniffles pcap generator',
     long_description='Packet capture generator for IDS evaluation',
     maintainer='Victor C. Valgenti',
